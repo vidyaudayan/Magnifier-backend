1d9a56208251ca8e5c2ef934d600d6d7107fe52c
vidyaudayan <vidyaudayan89@gmail.com>
2025-02-10 10:33:18 +0530

    emailverify

--- config/nodeMailer.js
+++ config/nodeMailer.js
@@ -5,16 +5,14 @@ dotenv.config();
 export const sendOtpEmail = async (email, otp) => {
     const transporter = nodemailer.createTransport({
         service: 'Gmail',
-        host: "smtp.gmail.com",
-        port: 587,
+        //host: "smtp.gmail.com",
+        //port: 587,
         secure: false, 
         auth: {
             user: process.env.EMAIL_USERNAME, // Add these to your .env
             pass: process.env.EMAIL_PASSWORD,
         },
-        tls: {
-            rejectUnauthorized: false, // Allow self-signed certificates (if needed)
-        },
+       
     });
 
     const mailOptions = {
@@ -27,7 +25,7 @@ export const sendOtpEmail = async (email, otp) => {
     try {
         await transporter.sendMail(mailOptions);
     } catch (error) {
-        console.error('Error sending email:', error);
+        console.error('Error sending email:', error.message || error);
         throw new Error('Failed to send email');
     }
 };
